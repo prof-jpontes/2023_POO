339717f2ea2c47dc9c0e3d16068bf450997958ed
José Cezar <116386868+Jcezar777@users.noreply.github.com>
2023-05-03 09:29:39 -0500

    Add files via upload

--- Programa.java
+++ Programa.java
@@ -1,39 +1,33 @@
-public class Programa{
-	public static void main(String[] args) {
-		Porta porta = new Porta();
-		porta.largura = 1.5;
-		porta.comprimento = 2.2;
-		porta.cor = "Branco";
-		porta.material = "Aço";
-		porta.abrir();
-		System.out.println("Dados da Primeira Porta:");
-		System.out.println("Largura = "+ porta.largura + "\nComprimento = " + porta.comprimento);
-		System.out.println(porta.fechada?"Porta estA fechada":"porta estA aberta");
-		System.out.println("Cor da Porta = " + porta.cor + "\nMaterial da porta = " + porta.material);
-		System.out.println(porta); //Mostrar endereço de memória da referência
+public class Programa {
+    public static void main(String[] args) {
+        Conta cezar = new Conta();
+        cezar.numAgencia = "11777";
+        cezar.numConta = "00001";
+        cezar.nomePessoa = "Cezar";
+        cezar.saldo = 1000;
 
-		Porta portaLab01 = new Porta();
-		portaLab01.largura = 1.0;
-		portaLab01.comprimento = 2.0;
-		portaLab01.cor = "Laranja Madeira";
-		portaLab01.material = "Madeira";
-		portaLab01.abrir();
-		System.out.println("\nDados da Segunda Porta:");
-		System.out.println("Largura = "+ portaLab01.largura + "\nComprimento = " + portaLab01.comprimento);
-		System.out.println(portaLab01.fechada?"Porta estA fechada":"porta estA aberta");
-		System.out.println("Cor da Porta = " + portaLab01.cor + "\nMaterial da porta = " + portaLab01.material);
-		System.out.println(portaLab01); //Mostrar endereço de memória da referência
+        System.out.println(cezar.getRealatorio());
 
-		Porta porta3 = new Porta();
-		porta3.largura = 1.0;
-		porta3.comprimento = 2.0;
-		porta3.cor = "Laranja Madeira";
-		porta3.material = "Madeira";
-		porta3.abrir();
-		System.out.println("\nDados da Terceira Porta:");
-		System.out.println("Largura = "+ porta3.largura + "\nComprimento = " + porta3.comprimento);
-		System.out.println(porta3.fechada?"Porta estA fechada":"porta estA aberta");
-		System.out.println("Cor da Porta = " + porta3.cor + "\nMaterial da porta = " + porta3.material);
-		System.out.println(porta3); //Mostrar endereço de memória da referência
-	}
+        if (cezar.sacar(500)){
+            System.out.println("Saque realizado!");
+        }else{
+            System.out.println("Saque não realizado!");
+        }
+
+        System.out.println("\nRelatório após saque: "+cezar.getRealatorio());
+
+        cezar.depositar(300);
+
+        System.out.println("\nRelatório após depósito: "+cezar.getRealatorio());
+
+        Conta elen = new Conta();
+        elen.numAgencia = "11777";
+        elen.numConta = "00012";
+        elen.nomePessoa = "Elen";
+        elen.saldo = 2109;
+
+        cezar.transferir(1000,elen);
+
+        System.out.println("\nRelatório após tranferência: "+cezar.getRealatorio()+elen.getRealatorio());
+    }
 }
